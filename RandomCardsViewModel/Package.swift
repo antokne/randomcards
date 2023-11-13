9c806278d986e7688ddf7555d7a4eb809879c90b
Ant Gardiner <antokne@gmail.com>
2023-11-13 15:09:58 +1300

    RandomCardsViewModel : package updates.

--- RandomCardsViewModel/Package.swift
+++ RandomCardsViewModel/Package.swift
@@ -4,20 +4,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "RandomCardsViewModel",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "RandomCardsViewModel",
-            targets: ["RandomCardsViewModel"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "RandomCardsViewModel"),
-        .testTarget(
-            name: "RandomCardsViewModelTests",
-            dependencies: ["RandomCardsViewModel"]),
-    ]
+	name: "RandomCardsViewModel",
+	platforms: [
+		.iOS(.v15),
+		.tvOS(.v15),
+		.macOS(.v13),
+	],
+	products: [
+		// Products define the executables and libraries a package produces, making them visible to other packages.
+		.library(
+			name: "RandomCardsViewModel",
+			targets: ["RandomCardsViewModel"]),
+	],
+	dependencies: [
+		.package(path: "./RandomCardsAPI"),
+		.package(path: "./RandomCardsModel"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.target(
+			name: "RandomCardsViewModel",
+			dependencies: [
+				.product(name: "RandomCardsAPI", package: "RandomCardsAPI"),
+				.product(name: "RandomCardsModel", package: "RandomCardsModel"),
+			]),
+		.testTarget(
+			name: "RandomCardsViewModelTests",
+			dependencies: ["RandomCardsViewModel"]),
+	]
 )
